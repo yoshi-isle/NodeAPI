56783f283100f965a0a4f179d4457aff1ba7d7b2
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-08-09 19:31:00 -0400

    get endpoint

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ app.listen(
     PORT,
     () => console.log(`it's running on http://localhost:${PORT}`)
 );
+
+app.get('/products', (request, result) =>
+{
+    result.status(200).send(
+        {
+            item: "coffee mug",
+            price: 0.23
+        }
+    )
+});
