588d3fbbe5ba738c34eb91dd0edf8f109509a99d
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-08-09 20:26:04 -0400

    Set up GET and POST requests to products

--- second_runthrough/app.js
+++ second_runthrough/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) =>
-{
-    res.status(200).json({
-        item:"hello"
-    });
-});
+const productRoutes = require('./routes/products');
+
+app.use('/products', productRoutes);
 
 module.exports = app;
