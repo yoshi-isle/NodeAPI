a7ab05f45d4ce6c6ac1e3abea2cb9648dea28dcf
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-08-09 21:19:06 -0400

    Add morgan to log requests

--- second_runthrough/app.js
+++ second_runthrough/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
