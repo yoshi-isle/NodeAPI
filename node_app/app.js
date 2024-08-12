121d128dbeb78ee863a04f25ef6af93ea5b82afa
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-08-11 20:38:44 -0400

    CORS stuff

--- node_app/app.js
+++ node_app/app.js
@@ -13,6 +13,16 @@ app.use(parser.urlencoded({
 }));
 app.use(parser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+    
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
